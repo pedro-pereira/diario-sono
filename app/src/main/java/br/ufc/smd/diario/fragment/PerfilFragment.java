d37332540be829b4556ff6fc3d3795006df80ac2
Pedro Pereira <pedrohenripereira@gmail.com>
2022-01-06 13:39:55 -0300

    Limpeza arquivos do TimePicker em modal

--- app/src/main/java/br/ufc/smd/diario/fragment/PerfilFragment.java
+++ app/src/main/java/br/ufc/smd/diario/fragment/PerfilFragment.java
@@ -1,38 +1,18 @@
 package br.ufc.smd.diario.fragment;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDialogFragment;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-
 import br.ufc.smd.diario.R;
 
-import android.app.Activity;
-import android.app.TimePickerDialog;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
 
 public class PerfilFragment extends Fragment {
 
-    TimePickerDialog picker;
-    EditText eText;
-    Button btnGet;
-    TextView tvw;
-
-    String selectedDate;
-
-    public static final int REQUEST_CODE = 11; // Used to identify the result
-
-
     public PerfilFragment() {
     }
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -40,51 +20,9 @@ public class PerfilFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_perfil, container, false);
-
-
-        // get fragment manager so we can launch from fragment
-        final FragmentManager fm = ((AppCompatActivity)getActivity()).getSupportFragmentManager();
-
-        // Using an onclick listener on the editText to show the datePicker
-
-        eText = view.findViewById(R.id.editText1);
-        eText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // create the datePickerFragment
-                AppCompatDialogFragment newFragment = new DatePickerFragment();
-                // set the targetFragment to receive the results, specifying the request code
-                newFragment.setTargetFragment(PerfilFragment.this, REQUEST_CODE);
-                // show the datePicker
-                newFragment.show(fm, "datePicker");
-            }
-        });
-
-        tvw = view.findViewById(R.id.textView1);
-
-        btnGet = view.findViewById(R.id.button1);
-        btnGet.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tvw.setText("Selected Time: "+ eText.getText());
-            }
-        });
-
         return view;
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // check for the results
-        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            // get date from string
-            selectedDate = data.getStringExtra("selectedDate");
-            // set the value of the editText
-            eText.setText(selectedDate);
-        }
     }
 }
